858e7a07866eff18096c8c77ac3e05f67c89d7d6
JOFIMOL JOHNSON <jofimolj@gmail.com>
2023-07-27 15:09:42 +0530

    finalcommit

---
+++ src/app/products/delete-product/delete-product.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-delete-product',
+  templateUrl: './delete-product.component.html',
+  styleUrls: ['./delete-product.component.css']
+})
+export class DeleteProductComponent implements OnInit {
+  productid:any
+
+  constructor(private activatedroute:ActivatedRoute,private productservice:ProductService,private router:Router) { }
+
+  ngOnInit(): void {
+    this.activatedroute.params.subscribe((data:any)=>{
+      this.productid=data['id']
+
+
+    })
+    this.productservice.deleteproduct(this.productid).subscribe((item:any)=>{
+alert("product deleted")
+this.router.navigateByUrl('products')
+
+    })
+  }
+
+}
---
+++ src/app/products/edit-product/edit-product.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-edit-product',
+  templateUrl: './edit-product.component.html',
+  styleUrls: ['./edit-product.component.css']
+})
+export class EditProductComponent implements OnInit {
+  productid:any
+  productdata:any
+
+  constructor(private activatedroute:ActivatedRoute,private productservice:ProductService,private router:Router ) { }
+
+  ngOnInit(): void {
+    this.activatedroute.params.subscribe((data:any)=>{
+      this.productid=data['id']
+    })
+    this.productservice.viewproduct(this.productid).subscribe((item:any)=>{
+      this.productdata=item
+    })
+  }
+  updateProduct(form:any){
+    let updateproduct={
+      id: form.value.id,
+      productName: form.value.productName,
+      categoryId: form.value.categoryId,
+      description: form.value.description,
+      price: form.value.price,
+      is_available: form.value.is_available,
+      productImg:form.value.productImg,
+      rating: form.value.rating,
+      review:form.value.review,
+      vendor_name: form.value.vendor_name,
+      warranty:form.value.warranty
+
+    }
+    this.productservice.editproduct(this.productid,this.productdata)
+    .subscribe((data:any)=>{
+      alert("updated product details")
+      this.router.navigateByUrl('products')
+    })
+
+  }
+
+}
